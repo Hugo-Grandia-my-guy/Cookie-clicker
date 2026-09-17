01b1c68d0a72f1960b44896fa3c396aa49f92dbc
Hugo-Grandia-my-guy <hugodepdep6@gmail.com>
2026-09-17 09:03:58 +0200

    factory kopen

--- script/clickCounter.js
+++ script/clickCounter.js
@@ -7,3 +7,17 @@ function clickCounter() {
     metersAmount = metersAmount + metersPerClick;
     document.getElementById('meterCounter').innerText = metersAmount;
 }
+
+const buyMarkRutteButton = document.getElementById("buyMarkRutteButton");
+
+let MarkRutte =0;
+let MarkRuttePrijs = 10
+
+
+
+buyMarkRutteButton.addEventListener("click",function () {
+    if (metersAmount >= MarkRutte) {
+        metersAmount = metersAmount - MarkRuttePrijs;
+        MarkRutte = MarkRutte + 4;
+    }
+});

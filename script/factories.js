111667c0cd19fd6559de2535c9ac0ad757935dee
Daniil <daniillishankov@gmail.com>
2026-09-25 14:08:24 +0200

    spell mistake fix & factories code readability

--- script/factories.js
+++ script/factories.js
@@ -46,17 +46,6 @@ class factory {
 }
 
 
-//      Add here a new Factories, also DON'T FORGET to add it in app.js    !!!
-//       1.   this.factories [...]
-//       2.   in render(){...} and
-//       3.   in setupEventListener(){...}
-
-const cursor = new factory("👈Cursor", 5, 1);
-const parkRuthe = new factory("🚴‍♀️Park Ruthe", 100, 10);
-const garage = new factory("🔳Garage", 5000, 250);
-const eBike = new factory("🔋e-Bike", 100000, 5000);
-
-
 // UPGRADES
 
 class cursorUpgrade extends factory {
@@ -81,9 +70,20 @@ class cursorUpgrade extends factory {
     }
 }
 
+//      Add here a new Factories, also DON'T FORGET to add it in app.js    !!!
+//       1.   this.factories [...]
+//       2.   in render(){...} and
+//       3.   in setupEventListener(){...}
+
+const cursor = new factory("👈Cursor", 5, 1);
+const parkRuthe = new factory("🚴‍♀️Park Ruthe", 100, 10);
+const garage = new factory("🔳Garage", 5000, 250);
+const eBike = new factory("🔋e-Bike", 100000, 5000);
+
 // Add here new upgrades
 
 const cursorUpgrade0 = new cursorUpgrade("cursorUpgrade0", 100, false);
 const cursorUpgrade1 = new cursorUpgrade("cursorUpgrade1", 1000, false);
 const cursorUpgrade2 = new cursorUpgrade("cursorUpgrade2", 10000, false);
 const cursorUpgrade3 = new cursorUpgrade("cursorUpgrade3", 100000, false);
+
